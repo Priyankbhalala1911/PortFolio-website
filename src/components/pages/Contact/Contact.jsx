4c25aba47e4e89c138456791782e3c5c46c18e8a
priya <priyankbhalala1911@gmail.com>
2024-06-08 17:38:14 +0530

    add contact details

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./Contact.css"
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const ref = useRef();
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -19,14 +21,24 @@ const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         // Handle form submission
+
+        emailjs.sendForm("service_wbfkikt", "template_rectr5m", ref.current, {
+            publicKey: "UVtCVq4Kseeiln1LD"
+        }).then(() => console.log("email sent successfully"),
+            (error) => { console.log(error.text) },
+            e.target.reset()
+        )
+            .catch((e) => console.log(e))
+
+        setFormData({name:"",email:"",message:""})
         console.log(formData);
     };
 
     return (
         <section className="contact" data-aos="zoom-out-right" data-aos-delay="25"
-        data-aos-duration="1000">
+            data-aos-duration="1000">
             <h2>Contact</h2>
-            <form className="contact-form" onSubmit={handleSubmit}>
+            <form className="contact-form" ref={ref} onSubmit={handleSubmit}>
                 <input data-aos="zoom-in-down"
                     type="text"
                     name="name"
